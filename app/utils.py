dfe7347e34e57d8facdd21a08a930aa921d2cec0
Abigail Ezell <maezell@gmail.com>
2026-07-12 20:37:30 -0500

    adding path guardrails to utils

--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,11 @@
 import streamlit as st
 import pandas as pd
 
-train_scaled = pd.read_pickle('./data/train_scaled.pkl')
+data_path = Path(__file__).parent / "data" / "train_scaled.pkl"
+train_scaled = pd.read_pickle(str(data_path))
+
+data_path = Path(__file__).parent / "data" / "train_unscaled.pkl"
 train_unscaled = pd.read_pickle('./data/train_unscaled.pkl')
+
+data_path = Path(__file__).parent / "data" / "test_data_with_pred.pkl"
 test = pd.read_pickle('./data/test_data_with_pred.pkl')
